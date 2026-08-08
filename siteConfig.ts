7276c01dd7e01d1dc135cf001511a19824c51d44
Flurin <flurin@MacBook-Air-von-Flurin.local>
2026-08-08 17:01:33 +0200

    DELETED: Suche auskommentiert, bis Indexer eingerichtet ist

--- siteConfig.ts
+++ siteConfig.ts
@@ -103,12 +103,12 @@ const getSiteConfig: SiteConfigProvider = () => {
                           </a>`
         },
         themeConfig: {
-            algolia: {
-                appId: 'P2ENEETR74',
-                apiKey: 'e5251468d5a81bb7569048e52f735999',
-                indexName: 'inf-gbsl',
-                searchPagePath: 'search'
-            },
+            // algolia: {
+            //   appId: '',
+            //   apiKey: '',
+            //   indexName: 'inf-trb',
+            //   searchPagePath: 'search'
+            // }
             docs: {
                 sidebar: {
                     hideable: true
